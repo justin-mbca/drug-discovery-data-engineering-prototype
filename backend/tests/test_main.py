c9eb0d8f48c6a4a66862f5e12041f3bc0c46d2ec
Justin Zhang <dlyzxl@yahoo.com>
2026-03-18 21:33:03 -0500

    Fix test import: use from backend.main import app

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,5 +1,8 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/../'))
 from fastapi.testclient import TestClient
-from main import app
+from backend.main import app
 
 def test_root():
     client = TestClient(app)
